f76ac48a27454abd9acef411099a0dbf5496b559
Anna <holdikana@gmail.com>
2015-02-09 18:54:18 -0500

    Added javascript

---
+++ js/general.js
@@ -0,0 +1,23 @@
+var blueberries = {
+    variety: 'Vaccinium corymbosum',
+    energy: '240 kJ',
+    carbohydrates: '14.49 g',
+    protein: '0.74 g',
+    growsOnShrub: true
+};
+
+var grapes = {
+    variety: 'Vitis vinifera',
+    energy: '288 kJ',
+    carbohydrates: '18.1 g',
+    protein: '0.72 g',
+    growsOnShrub: false
+};
+
+var redCurreant = {
+    variety: 'Ribes rubrum',
+    energy: '234 kJ',
+    carbohydrates: '13.8 g',
+    protein: '1.4 g',
+    growsOnShrub: true
+};
